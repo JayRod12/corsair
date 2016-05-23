51e1175ff2a7b6f70e041b25443cfa61569788f8
Jaime Rodriguez <jaime.rodriguez13@imperial.ac.uk>
2016-05-23 15:52:10 +0200

    Resolve paths locally before sending a file
    Otherwise express throws forbidden errors. It
    is wary of path traversals such as ‘../‘

--- server_scripts/app.js
+++ server_scripts/app.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var path = require('path');
 //var socket = require('socket.io')(http);
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(path.resolve(__dirname + '/../public/')));
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(path.resolve(__dirname + '/../html/index.html'));
 });
 
 app.get('/game', function(req, res) {
-  res.sendFile(__dirname + '/html/game.html');
+  res.sendFile(path.resolve(__dirname + '/../html/game.html'));
 });
 
 http.listen(3000, function() {
