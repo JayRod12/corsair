254f1099d724688bae2ea7d8dfbcd94ea2066691
Dan Slocombe <ds4314@ic.ac.uk>
2016-05-24 18:34:57 +0100

    Now have all the functionality we had yesterday (i.e. rotation and velocity mechanics) but in pure JS with canvas

--- client/game.js
+++ client/game.js
@@ -1,77 +1,107 @@
+var canvas = $("#canvas")[0];
+var ctx = canvas.getContext("2d");
+var w = $("#canvas").width();
+var h = $("#canvas").height();
 
-/* TODO make framerate independent - add deltatime to tick updates */
+//  Array of objects to be updated every tick
+var gameObjects = [];
+var drawObjects = [];
 
-const speed_norm = 100;
-const max_angular_vel = 0.1;
 
-var sea = new Rect(0, 0, 1000, 1000).fill("blue").addTo(stage);
+//  Called fps times a second, dt is delta time
+function tick(dt){
+  //  Updates all game object with their onTick functions
+  for (var i = 0; i < gameObjects.length; i++){
+    gameObjects[i].onTick(dt);
+  }
+  draw();
+}
 
-function Ship(x, y){
-  this.width = 40;
-  this.height = 20;
+//  Draws all objects
+function draw(){
+  //  Fastest way to clear entire canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "blue";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  for (var i = 0; i < drawObjects.length; i++){
+    drawObjects[i].onDraw();
+  }
+  //fixed object to ensure no hickery dickery 
+  
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, 50, 60);
+  ctx.fillRect(400, 100, 120, 60);
 
-  this.speed = 1;
+}
+
+//  Where inputFunction is the ships method of input, either a function
+//  taking information from the server or local input (mouse)
+
+
+function Ship(x, y, inputFunction){
+
+  //  Where x and y are the spawning position
+  this.x = x;
+  this.y = y;
+
+  this.width = 90;
+  this.height = 40;
+
+  this.speed = 5;
   this.angle = 0;
 
-  this.shape = new Ellipse(0, 0, this.width, this.height);
-  this.shape.fill("white");
+  this.inputFunction = inputFunction;
 
-  this.setLocation(x, y);
-  this.shape.addTo(stage);
-};
+  this.onTick = function(dt){
 
-Ship.prototype.move = function(){
-  player.updateAngle(mouse_x, mouse_y);
-  this.speed = Math.sqrt(Math.pow(this.x - mouse_x,2) + Math.pow(this.y -
-        mouse_y,2)) / speed_norm;
-  this.setLocation(this.x + Math.cos(this.angle) * this.speed,
-              this.y + Math.sin(this.angle) * this.speed);
-};
+    //  Updates speed and angle
+    this.inputFunction();
 
-Ship.prototype.updateAngle = function(mouse_x, mouse_y){
-
-  this.angle = Math.atan2(mouse_y - this.y, mouse_x - this.x);
-
-  /*
-  Try and add 'momentum' to angular velocity, so you cant spin on the spot
-  currently spins 360's
-  Fix, do some modular arithmatic, 
-  var newAngle = Math.atan2(mouse_y - this.y, mouse_x - this.x);
-  if (Math.abs(newAngle - this.angle) > max_angular_vel){
-    this.angle += (newAngle > 0) ? max_angular_vel : -max_angular_vel;
+    this.x += this.speed * Math.cos(this.angle) * dt;
+    this.y += this.speed * Math.sin(this.angle) * dt;
   }
-  else{
-    this.angle = newAngle;
+  this.onDraw = function(){
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.angle);
+
+    //  For now draw square
+    ctx.fillStyle = "brown";
+    ctx.fillRect(- this.width/2, - this.height/2, this.width, this.height);
+    ctx.rotate(-this.angle);
+    ctx.translate(-this.x, -this.y);
+
   }
-  */
-
-  this.shape.attr({
-    rotation: this.angle
-  });
-};
-
-Ship.prototype.setLocation = function(x, y){
-  this.x = x;
-  this.y = y; 
-
-
-  this.shape.attr({
-    x: x - this.width/2,
-    y: y - this.height/2,
-  });
-};
-
-var player = new Ship(50, 60);
+}
 
 var mouse_x = 0;
 var mouse_y = 0;
 
-stage.on('tick', function(e, f) {
-  player.move();
+//  Update mouse position on movement
+$( "#canvas" ).mousemove(function(event){
+//canvas.mousemove(function(event){
+  mouse_x = event.clientX;
+  mouse_y = event.clientY;
 });
 
-stage.on('pointermove', function(e) {
+const speed_norm = 100/60;
 
-  mouse_x = e.stageX;
-  mouse_y = e.stageY;
-});
+var playerInput = function(){
+  this.angle = Math.atan2(mouse_y - this.y, mouse_x - this.x);  
+  this.speed = Math.sqrt(Math.pow(this.x - mouse_x,2) + Math.pow(this.y
+      -mouse_y,2)) / speed_norm;
+}
+
+function init(){
+  var player = new Ship(20, 20, playerInput);
+  gameObjects.push(player);
+  drawObjects.push(player);
+
+  //  Todo measure dt properly every tick to compensate for dropped frames
+  const dt = 1/60;
+
+  if(typeof game_loop != "undefined") clearInterval(game_loop);
+  //  Third argument is the delay time to pass to tick on each call
+  var game_loop = setInterval(tick, dt, dt);
+}
+init();
+
