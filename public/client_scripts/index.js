d82bdb6aad0c76fce4f057d453e6fbadd9025366
Nacho <ignacio.navarro14@imperial.ac.uk>
2016-05-29 10:51:17 +0100

    Welcome page now uses cookies so that the fade in only occurs once. Also, I've removed the js animation for old browsers that don't support webGL.

--- public/client_scripts/index.js
+++ public/client_scripts/index.js
@@ -1,7 +1,26 @@
-$(document).ready(function () {
-     // Fade in animation
-     $("body").hide().fadeIn(3000); 
-     $("div div canvas").fadeIn(1000);
-     $('#trophyButton').hide().fadeIn(1000); 
+function webgl_support() { 
+   try {
+    var canvas = document.createElement( 'canvas' ); 
+    return !! window.WebGLRenderingContext && canvas.getContext('webgl');
+   } catch(e) { 
+   	return false; 
+   } 
+ };
 
+
+// For old browsers, replace the boat animation with just the title.
+if (!webgl_support()) {
+	window.onload = function() {
+    	document.getElementById("titleCorsair").innerHTML = "CORSAIR";
+	}
+}
+
+
+$(document).ready(function () {
+	$(function () {
+  		if ($.cookie("loaded") != "true") {
+      		$('body.home').hide().fadeIn(3000);
+      		$.cookie("loaded", "true");
+ 	 	}
+	});
 });
