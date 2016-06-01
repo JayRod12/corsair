a499ddcad3825d60ddcfe5f080f5d7e4eabcb406
Jaime Rodriguez <jaime.rodriguez13@imperial.ac.uk>
2016-06-01 18:27:48 +0100

    Check for null cell

--- server_scripts/app.js
+++ server_scripts/app.js
@@ -160,20 +160,22 @@ function calculateCellsToSend(uid){
   var s = Game.getPlayerShips()[uid];
   var list = [];
   var base = sim.coordinateToCellIndex(s.state.x,s.state.y);
-  const bufferConst = 4;
-  if (base.x + 1 < gridNumber && s.state.x % cellWidth > cellWidth/bufferConst){
-    list.push({x:base.x+1, y:base.y})
+  if (base != null) {
+    const bufferConst = 4;
+    if (base.x + 1 < gridNumber && s.state.x % cellWidth > cellWidth/bufferConst){
+      list.push({x:base.x+1, y:base.y})
+    }
+    if (base.y + 1 < gridNumber && s.state.y % cellHeight > cellHeight/bufferConst){
+      list.push({x:base.x, y:base.y+1})
+    }
+    if (base.x - 1 > 0 && s.state.x % cellWidth < cellWidth/bufferConst){
+      list.push({x:base.x-1, y:base.y})
+    }
+    if (base.y - 1 > 0 && s.state.y % cellHeight < cellHeight/bufferConst){
+      list.push({x:base.x, y:base.y-1})
+    }
+    list.push(base);
   }
-  if (base.y + 1 < gridNumber && s.state.y % cellHeight > cellHeight/bufferConst){
-    list.push({x:base.x, y:base.y+1})
-  }
-  if (base.x - 1 > 0 && s.state.x % cellWidth < cellWidth/bufferConst){
-    list.push({x:base.x-1, y:base.y})
-  }
-  if (base.y - 1 > 0 && s.state.y % cellHeight < cellHeight/bufferConst){
-    list.push({x:base.x, y:base.y-1})
-  }
-  list.push(base);
   return list;
 }
 
