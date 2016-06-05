2513ba3eded05ad53dac4fcb0bd4c9f549c06e11
Jaime Rodriguez <jaime.rodriguez13@imperial.ac.uk>
2016-06-05 17:20:52 +0100

    Law case and project technology

--- tests/test.js
+++ tests/test.js
@@ -13,14 +13,14 @@ describe('Collision detection - positive cases:', function(){
     var r1 = pos_cases[i].r1;
     var r2 = pos_cases[i].r2;
     it('Should collide_'+i.toString(), function(){
-      assert(Collisions.collisionDetection(r1, r2, true), 
+      assert(Collisions.RectRect(r1, r2, true),
         'Isn\'t fucking colliding where it should be');
     });
   }
 
   it('Should collide with itself', function(){
     var r1 = pos_cases[0].r1;
-    assert(Collisions.collisionDetection(r1, r1, true), 
+    assert(Collisions.RectRect(r1, r1, true),
       'Isn\'t colliding with itself, wtf?');
   });
 });
@@ -32,7 +32,7 @@ describe('Collision detection - negative cases:', function(){
     assert(r1.x);
     var r2 = neg_cases[i].r2;
     it('Shouldn\'t collide_'+i.toString(), function(){
-      assert(!Collisions.collisionDetection(r1, r2, true),
+      assert(!Collisions.RectRect(r1, r2, true),
         'Is colliding when the objects don\'t overlap, what are you doing Tom?');
     });
   }
