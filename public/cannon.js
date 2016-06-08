3c5d82d71a1039768ffa475f96682c3cff467634
Thomas Grigg <tgg14@ray28.doc.ic.ac.uk>
2016-06-08 18:59:31 +0100

    got cannons working with arrays instead

--- public/cannon.js
+++ public/cannon.js
@@ -10,33 +10,77 @@ else{
 
 (function(exports){
 
-function Cannon(ship) {
+var initialCannons = 10;
 
-  this.ballSpeed = 0.2;
-  this.cannons = 12;
+
+function Cannons(ship) {
+
+  //left and right arrays of cannons
+  this.leftCannons = new Array();
+  this.rightCannons = new Array();
+
+  //cannon and cannonball params
+  //this.cannons = 12;
+  this.cannonNumber = initialCannons;
+  this.ballSpeed = 0.4;
   this.spacing = 10;
-  this.delay = 10;
+  this.delay = 200;
+
+  for (var i = 0; i < initialCannons; i++) {
+    this.leftCannons.push(new SingleCannon(i, 1, ship, this));
+    this.rightCannons.push(new SingleCannon(i, -1, ship, this)); 
+  }
+
+ 
+
+  
+  
+  //owner of these cannons
   this.ship = ship;
   this.level = 2;
 
   this.baseCooldown = 1200;
   this.cooldowns = [10, 10];
 
-  this.futureShots = [];  //  List of future firing events
+  //this.futureShots = [];  //  List of future firing events
 
   this.onShoot = function(side) {
 
-    //Ask server if we are allowed to shoot (MaybeTODO)
-
-    var index;
+    //Ask server if we are allowed to shoot (MaybeTODO), DAN: why do we do this?
     var index = ((side == 1) ? 0 : 1);
+
+    var cannonArray;
+    if (index == 0) {
+      cannonArray = this.leftCannons;
+    } else {
+      cannonArray = this.rightCannons;
+    }
+
     var cooldown = this.cooldowns[index];
 
     if (cooldown > 0) return false;
 
+    //otherwise, we're firing!
     this.cooldowns[index] = this.baseCooldown;
 
+    for (var i = 0; i < cannonArray.length; i++) {
+      var fire_delay = Math.random()*this.delay;
+      //tell each 
+      cannonArray[i].fire(fire_delay);
+    } 
+    
 
+
+
+
+
+
+
+
+
+
+
+    /*
     for (var i = 0; i < this.cannons; i++){
 
       var ship = this.ship;
@@ -57,13 +101,21 @@ function Cannon(ship) {
 
       this.futureShots.push({time: i*this.delay, f : shot, i: i});
     }
-
+*/
   };
 
   this.onTick = function(dt){
     if (this.cooldowns[0] > 0) this.cooldowns[0] -= dt;
     if (this.cooldowns[1] > 0) this.cooldowns[1] -= dt;
 
+    for (var i = 0; i < this.leftCannons.length; i++) {
+      this.leftCannons[i].onTick(dt);
+    }
+
+    for (var i = 0; i < this.rightCannons.length; i++) {
+      this.rightCannons[i].onTick(dt);
+    }
+/*
     for (var i = 0; i < this.futureShots.length; i++){
       //  Inefficient?
       //this.futureShots[i] = {time: this.futureShots[i].time - dt, f:
@@ -74,9 +126,12 @@ function Cannon(ship) {
         this.futureShots.splice(i,1);
         i = i - 1;
       }
-    }
+    }*/
   };
+
+	
 }
+
 
 function cannonBallFromLocal(ship, offsetX, offsetY, side, ballSpeed, level){
   var sim = ship.sim;
@@ -143,6 +198,7 @@ function CannonBall(sim, uid, state, level) {
     ctx.arc(this.state.x, this.state.y, radius, 2 * Math.PI, false);
     ctx.fillStyle = "black";
     ctx.fill();
+//debugger;
   }
   this.serialize = function() {
     return {type: "cannonball",
@@ -155,8 +211,41 @@ function CannonBall(sim, uid, state, level) {
   }
 }
 
+function SingleCannon(index, side, ship, handler) {
+  this.index = index;
+  this.side = side;
+  this.ship = ship;
+  this.fired = false;
+  
+  this.fire_timer = 0;
+  
+  this.fire = function(fire_delay) {
+   // console.log("holy shit did that acutally work first time");
+    this.fire_timer = fire_delay;
+    this.fired = true;
+  }
+  
+  this.onTick = function(dt) {
+    //console.log("NOW WE ARE HERE");
+    if (this.fired) {
+     if (this.fire_timer > 0) {
+       this.fire_timer -= dt;
+     } else {
+        var offsetX = handler.spacing * (handler.cannonNumber - this.index) * Math.cos(ship.state.angle);
+        var offsetY = handler.spacing * (handler.cannonNumber - this.index) * Math.sin(ship.state.angle);
 
-exports.Class = Cannon;
+   //     console.log(ship.level);
+        var ball = cannonBallFromLocal(ship, offsetX, offsetY, this.side, handler.ballSpeed, handler.level);
+         toSendServer.push(ball.serialize());
+        var cell = ship.sim.coordinateToCell(ship.state.x,ship.state.y);
+        cell.gameObjects.push(ball);
+        this.fired = false;
+     } 
+    }
+  }
+}
+
+exports.Class = Cannons;
 exports.CannonBall = CannonBall;
 
 })(typeof exports == 'undefined' ? this.Cannon = {} : exports);
